6a877253f12ec6d78639863a55dc3a4accbe1ec7
hyxklee <ewgt1234@naver.com>
2024-09-25 10:56:40 +0900

    feat: comment 도메인 생성

---
+++ src/main/java/com/leets/X/domain/comment/domain/Comment.java
@@ -0,0 +1,32 @@
+package com.leets.X.domain.comment.domain;
+
+import com.leets.X.domain.post.domain.Post;
+import com.leets.X.domain.user.domain.User;
+import com.leets.X.global.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne // 다 대 일 매핑
+    @JoinColumn(name = "user_id") // user_id 컬럼을 통해 join을 하겠다
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="post_id")
+    private Post post;
+
+}
+
