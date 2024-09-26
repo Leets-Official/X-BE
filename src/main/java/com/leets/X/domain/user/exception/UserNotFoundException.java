95c921e90463e1b13e17c3c072ef0f486f8549f6
hyxklee <ewgt1234@naver.com>
2024-09-26 20:08:29 +0900

    feat: UserNotFoundException 구현

---
+++ src/main/java/com/leets/X/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.leets.X.domain.user.exception;
+
+import com.leets.X.global.exception.BaseException;
+
+import static com.leets.X.domain.user.exception.ErrorMessage.*;
+
+// 모든 사용자 정의 예외는 BaseException을 상속할 것
+public class UserNotFoundException extends BaseException {
+    public UserNotFoundException() {
+        super(USER_NOT_FOUND.getCode(), USER_NOT_FOUND.getMessage());
+    }
+
+}
