26308e175dbc6cecf0688dab65ca7ef93d7574be
koreaioi <koreaioi@gachon.ac.kr>
2024-09-30 20:39:32 +0900

    feat: PublishMessage 도메인 추가

---
+++ src/main/java/com/leets/X/domain/chat/redis/PublishMessage.java
@@ -0,0 +1,37 @@
+package com.leets.X.domain.chat.redis;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+/*
+* PublishMessage
+* Redis의 pub/sub 과정 데이터를 주고 받을 때, 사용할 직렬화 클래스
+* 24.09.30 - 역직렬화 에러를 해결하지 못하고 있음
+* */
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublishMessage {
+
+
+    private static final long serialVersionUID = 2082503192322391880L;
+
+    @NotNull
+    private Long roomId;
+    @NotNull
+    private Long senderId;
+    @NotNull
+    private String content;
+
+//
+//    LocalDateTime 역직렬화 에러
+//    @JsonSerialize(using = LocalDateTimeSerializer.class)
+//    @JsonDeserialize(using = LocalDateDeserializer.class)
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime createAt;
+//
+//
+}
