83d945ca062f6770d4a3ab1d23be31356ea21e31
koreaioi <koreaioi@gachon.ac.kr>
2024-09-30 20:45:43 +0900

    feat: RedisSubscriber 추가

---
+++ src/main/java/com/leets/X/domain/chat/redis/RedisSubscriber.java
@@ -0,0 +1,39 @@
+package com.leets.X.domain.chat.redis;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RedisSubscriber implements MessageListener{
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper obejctMapper;
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        // 역 직렬화 문자열
+        String tmpMessage = redisTemplate.getStringSerializer().deserialize(message.getBody());
+
+        log.info("구독자 전송 전 message: {}", tmpMessage);
+        PublishMessage publishMessage = null;
+        try {
+            publishMessage = obejctMapper.readValue(tmpMessage, PublishMessage.class);
+            messageTemplate.convertAndSend("/sub/chats" + publishMessage.getRoomId(), publishMessage);
+            log.info("구독자 전송 후 message: {}", publishMessage.getContent());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
