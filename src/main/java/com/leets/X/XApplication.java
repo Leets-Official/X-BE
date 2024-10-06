68d7e5ee9aef91ef80614caf7a41a5755be3b244
hyxklee <ewgt1234@naver.com>
2024-10-06 14:31:00 +0900

    feat: baseTimeEntity 사용을 위한 어노테이션 추가

--- src/main/java/com/leets/X/XApplication.java
+++ src/main/java/com/leets/X/XApplication.java
@@ -2,8 +2,10 @@ package com.leets.X;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class XApplication {
 
 	public static void main(String[] args) {
