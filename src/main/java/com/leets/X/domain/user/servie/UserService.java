6d0efa91d7a89fa82f8beed56a4cac80168f1e26
hyxklee <ewgt1234@naver.com>
2024-10-06 14:30:29 +0900

    feat: 소셜 로그인 성공 시 로그인, 회원가입을 구분해 로직 실행

--- src/main/java/com/leets/X/domain/user/servie/UserService.java
+++ src/main/java/com/leets/X/domain/user/servie/UserService.java
@@ -1,12 +1,67 @@
 package com.leets.X.domain.user.servie;
 
+import com.leets.X.domain.user.domain.User;
+import com.leets.X.domain.user.dto.response.UserSocialLoginResponse;
+import com.leets.X.domain.user.exception.UserNotFoundException;
+import com.leets.X.domain.user.repository.UserRepository;
+import com.leets.X.global.auth.google.AuthService;
+import com.leets.X.global.auth.google.dto.GoogleTokenResponse;
+import com.leets.X.global.auth.google.dto.GoogleUserInfoResponse;
+import com.leets.X.global.auth.jwt.JwtProvider;
+import com.leets.X.global.auth.jwt.JwtResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import static com.leets.X.domain.user.domain.enums.LoginStatus.LOGIN;
+import static com.leets.X.domain.user.domain.enums.LoginStatus.REGISTER;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class UserService {
+
+    private final AuthService authService;
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserSocialLoginResponse authenticate(String authCode) {
+        GoogleTokenResponse token = authService.getGoogleAccessToken(authCode);
+        GoogleUserInfoResponse userInfo = authService.getGoogleUserInfo(token.access_token());
+
+        // 가입된 유저라면 로그인
+        if (userRepository.existsByEmail(userInfo.email())){
+            return loginUser(userInfo.email());
+        }
+        // 아니라면 회원가입
+        return registerUser(userInfo);
+    }
+
+    private UserSocialLoginResponse loginUser(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        return new UserSocialLoginResponse(user.getId(), LOGIN, generateToken(email));
+    }
+
+    private UserSocialLoginResponse registerUser(GoogleUserInfoResponse userInfo) {
+        User user = User.builder()
+                .name(userInfo.name())
+                .email(userInfo.email())
+                .build();
+
+        userRepository.save(user);
+
+        return new UserSocialLoginResponse(user.getId(), REGISTER, generateToken(user.getEmail()));
+    }
+
+    private JwtResponse generateToken (String email){
+        return JwtResponse.builder()
+                .accessToken(jwtProvider.generateAccessToken(email))
+                .refreshToken(jwtProvider.generateRefreshToken())
+                .build();
+    }
 
 }
