25da7ac307cb523e6afb1befbf38b5b0562a9e69
seokjun01 <suckjungod@naver.com>
2024-10-29 00:09:27 +0900

    Refactor :  @JsonCreator로 Json데이터를 객체로 변환

--- src/main/java/com/leets/X/domain/post/dto/request/PostRequestDTO.java
+++ src/main/java/com/leets/X/domain/post/dto/request/PostRequestDTO.java
@@ -1,13 +1,21 @@
 package com.leets.X.domain.post.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Builder
 @Getter
 public class PostRequestDTO {
 
+    @JsonCreator
+    public PostRequestDTO(@JsonProperty("content") String content) {
+        this.content = content;
+    }
     @NotBlank(message = "Content cannot be empty")
     private String content;
 
