05d01e5229e427202d954633ee405bf7b26a8d4f
seokjun01 <suckjungod@naver.com>
2024-10-30 03:04:53 +0900

    Refactor: post_id 컬럼을 통해 join해줌

--- src/main/java/com/leets/X/domain/like/domain/Like.java
+++ src/main/java/com/leets/X/domain/like/domain/Like.java
@@ -1,21 +1,26 @@
-package com.leets.X.domain.like.domain;
+    package com.leets.X.domain.like.domain;
 
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+    import com.leets.X.domain.post.domain.Post;
+    import jakarta.persistence.*;
+    import lombok.AccessLevel;
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.NoArgsConstructor;
 
-@Entity
-// mysql 예약어이기 때문에 별도 지정
-@Table(name = "likes")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
-public class Like {
+    @Entity
+    // mysql 예약어이기 때문에 별도 지정
+    @Table(name = "likes")
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Builder
+    public class Like {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "like_id")
-    private Long id;
-}
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "like_id")
+        private Long id;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "postId")
+        private Post post;
+    }
