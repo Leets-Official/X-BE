82d553fdc4ed78b31ae666012e4be4b08a134f85
koreaioi <koreaioi@gachon.ac.kr>
2024-10-30 00:15:36 +0900

    feat: ChatRoomRepository 구현

---
+++ src/main/java/com/leets/X/domain/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package com.leets.X.domain.chat.repository;
+
+import com.leets.X.domain.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+}
