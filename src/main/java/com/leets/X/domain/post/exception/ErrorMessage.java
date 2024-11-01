8cbd31b5d4f7defba02df3d22a8da20b792c87ae
seokjun01 <suckjungod@naver.com>
2024-11-01 23:06:21 +0900

    Feat : 에러메시지와 , 개시글이 존재하지 않는 예외처리

--- src/main/java/com/leets/X/domain/post/exception/ErrorMessage.java
+++ src/main/java/com/leets/X/domain/post/exception/ErrorMessage.java
@@ -1,4 +1,15 @@
 package com.leets.X.domain.post.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ErrorMessage {
+
+    POST_NOT_FOUND(404,"존재하지 않는 게시글입니다.");
+
+    private final int code;
+    private final String message;
+
 }
--- src/main/java/com/leets/X/domain/post/exception/PostNotFoundException.java
+++ src/main/java/com/leets/X/domain/post/exception/PostNotFoundException.java
@@ -1,9 +1,14 @@
 package com.leets.X.domain.post.exception;
 
-public class PostNotFoundExceptiom {
-    public class PostNotFoundException extends RuntimeException {
-        public PostNotFoundException(String message) {
-            super(message);
-        }
+public class PostNotFoundException extends RuntimeException {
+
+    public PostNotFoundException() {
+        super("해당 게시글을 찾을 수 없습니다.");
     }
+
+    // 사용자 정의 메시지를 받을 수 있는 생성자
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+
 }
