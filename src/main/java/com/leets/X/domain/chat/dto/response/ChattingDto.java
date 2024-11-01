fc1201b56887236c0a47c418efb2dcf1cc045b88
koreaioi <koreaioi@gachon.ac.kr>
2024-11-02 01:53:38 +0900

    feat:  ChattingDto 구현 (한개의 채팅방 DTO)

---
+++ src/main/java/com/leets/X/domain/chat/dto/response/ChattingDto.java
@@ -0,0 +1,12 @@
+package com.leets.X.domain.chat.dto.response;
+
+import java.util.List;
+
+public record ChattingDto(
+    Long user1Id,
+    Long user2Id,
+    String user1Name,
+    String user2Name,
+    List<ChatMessageResponseDto> chatMessageList
+) {
+}
