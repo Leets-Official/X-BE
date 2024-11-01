bfda80ecae7480313e3b23c1ce67da1a73988a87
koreaioi <koreaioi@gachon.ac.kr>
2024-11-02 01:58:16 +0900

    feat: ChatRoomController 구현

--- src/main/java/com/leets/X/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/leets/X/domain/chat/controller/ChatRoomController.java
@@ -1,8 +1,10 @@
 package com.leets.X.domain.chat.controller;
 
 
+import com.leets.X.domain.chat.dto.request.ChatRoomCheckRequstDto;
 import com.leets.X.domain.chat.dto.request.ChatRoomRequestDto;
 import com.leets.X.domain.chat.dto.response.ChatRoomResponseDto;
+import com.leets.X.domain.chat.dto.response.ChattingDto;
 import com.leets.X.domain.chat.service.ChatRoomService;
 import com.leets.X.global.common.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import static com.leets.X.domain.chat.controller.ResponseMessage.*;
+
 @RestController
 @Slf4j
 @Validated
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/chatRoom")
 @RequiredArgsConstructor
 public class ChatRoomController {
 
@@ -25,19 +29,33 @@ public class ChatRoomController {
 
     }
 
-    @PostMapping("/chatRoom")
+    @PostMapping
     public ResponseDto<ChatRoomResponseDto> createChatRoom(@RequestBody ChatRoomRequestDto chatRoomRequestDto){
         ChatRoomResponseDto response = chatRoomService.save(chatRoomRequestDto);
-        return ResponseDto.response(200, "Successfully create ChatRoom!", response);
+        return ResponseDto.response(CHATROOM_CREATE_SUCCESS.getCode(), CHATROOM_CREATE_SUCCESS.getMessage(), response);
     }
 
-    // 채팅방 하나를 조회해준다. (대화 내용을 돌려준다는 의미)
-    @GetMapping("/chatRoom")
-    public ResponseDto<ChatRoomResponseDto> findChatRoom(
+    // 채팅방 하나를 조회해준다. (대화 내역을 돌려준다는 의미)
+    @GetMapping
+    public ResponseDto<ChattingDto> findChatRoom(
                                     @RequestParam Long roomId, @RequestParam int size, @RequestParam int page ){
-        chatRoomService.findByChatRoom(roomId, size, page);
-        ChatRoomResponseDto response = new ChatRoomResponseDto(1L);
-        return ResponseDto.response(200, "Successfully return ChatRoom", response);
+        ChattingDto response = chatRoomService.findByChatRoom(roomId, size, page);
+        return ResponseDto.response(GET_CHATROOM.getCode(), GET_CHATROOM.getMessage(), response);
+    }
+
+    // user1Id와 user2Id의 채팅방이 있는 지 조회
+    @GetMapping("/user")
+    public ResponseDto<ChatRoomResponseDto> existChatRoom(@RequestBody ChatRoomCheckRequstDto checkDto){
+        ChatRoomResponseDto response = chatRoomService.findUser1User2ChatRoom(checkDto);
+
+        return ResponseDto.response(GET_ROOMID.getCode(), GET_ROOMID.getMessage(), response);
+
+    }
+
+    @PostMapping("/test") // addListener 테스트 용
+    public void addListener(@RequestParam Long roomId) {
+        chatRoomService.addListener(roomId);
+        log.info("addListener");
     }
 
 }
