d31f6ef63e41042e1600a98861dc05f29bcac128
koreaioi <koreaioi@gachon.ac.kr>
2024-11-02 01:19:05 +0900

    feat: MongoRespositoy 추가

---
+++ src/main/java/com/leets/X/domain/chat/repository/ChatMessageRepository.java
@@ -0,0 +1,10 @@
+package com.leets.X.domain.chat.repository;
+
+import com.leets.X.domain.chat.entity.ChatMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+    
+}
