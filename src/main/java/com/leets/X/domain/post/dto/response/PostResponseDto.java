0270afa189ddd4eb4c3aa61ffde6dae870eb81e1
seokjun01 <suckjungod@naver.com>
2024-11-01 23:03:39 +0900

    Refactor: postResponseDto에서 정적팩토리 메서드에서 , 각 필드를 반환할때, null의 여부를 검토하여 적절히 처리함

--- src/main/java/com/leets/X/domain/post/dto/response/PostResponseDto.java
+++ src/main/java/com/leets/X/domain/post/dto/response/PostResponseDto.java
@@ -1,6 +1,8 @@
 package com.leets.X.domain.post.dto.response;
 
 
+import com.leets.X.domain.comment.domain.Comment;
+import com.leets.X.domain.image.domain.Image;
 import com.leets.X.domain.post.domain.Post;
 import com.leets.X.domain.post.domain.enums.IsDeleted;
 import lombok.AllArgsConstructor;
@@ -8,6 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 @Getter
@@ -30,9 +33,12 @@ public class PostResponseDto {
                 post.getViews(),
                 post.getIsDeleted(),
                 post.getCreatedAt(),
-                UserResponseDto.from(post.getUser()), // UserResponseDto 변환
-                post.getImages().stream().map(ImageResponseDto::from).toList(), // 이미지 리스트 변환
-                post.getCommentList().stream().map(CommentResponseDto::from).toList() // 댓글 리스트 변환
+                post.getUser() != null ? UserResponseDto.from(post.getUser()) : null, // null의 여부를 확인하여 null 일 떄 적절히 처리가능
+                (post.getImages() != null ? post.getImages() : List.<Image>of())
+                        .stream().map(ImageResponseDto::from).toList(), // 이미지 리스트 변환
+                (post.getCommentList() != null ? post.getCommentList() : List.<Comment>of())
+                        .stream().map(CommentResponseDto::from).toList() // 댓글 리스트 변환
         );
     }
+
 }
