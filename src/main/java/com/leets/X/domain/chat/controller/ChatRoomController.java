9dbd7aae13f6d8c13aa4578f6d1e3df9a4b28c69
koreaioi <koreaioi@gachon.ac.kr>
2024-11-03 02:54:19 +0900

    fix: ChatRoom 책임 분리

--- src/main/java/com/leets/X/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/leets/X/domain/chat/controller/ChatRoomController.java
@@ -2,11 +2,8 @@ package com.leets.X.domain.chat.controller;
 
 
 import com.leets.X.domain.chat.dto.request.ChatRoomCheckRequstDto;
-import com.leets.X.domain.chat.dto.request.GetChatRoomRequestDto;
 import com.leets.X.domain.chat.dto.request.FindChatRoomRequestDto;
 import com.leets.X.domain.chat.dto.response.ChatRoomResponseDto;
-import com.leets.X.domain.chat.dto.response.ChattingDto;
-import com.leets.X.domain.chat.dto.response.ChattingListResponseDto;
 import com.leets.X.domain.chat.service.ChatRoomService;
 import com.leets.X.global.common.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 import static com.leets.X.domain.chat.controller.ResponseMessage.*;
 
@@ -33,13 +29,7 @@ public class ChatRoomController {
         return ResponseDto.response(CHATROOM_CREATE_SUCCESS.getCode(), CHATROOM_CREATE_SUCCESS.getMessage(), response);
     }
 
-    // 채팅방 하나를 조회해준다. (대화 내역을 돌려준다는 의미)
-    @GetMapping
-    public ResponseDto<ChattingDto> findChatRoom(
-            @RequestBody GetChatRoomRequestDto getChatRoomRequestDto){
-        ChattingDto response = chatRoomService.getChatRoom(getChatRoomRequestDto);
-        return ResponseDto.response(GET_CHATROOM.getCode(), GET_CHATROOM.getMessage(), response);
-    }
+
 
     // user1Id와 user2Id의 채팅방이 있는 지 조회
     @GetMapping("/users")
@@ -49,13 +39,6 @@ public class ChatRoomController {
         return ResponseDto.response(GET_ROOMID.getCode(), GET_ROOMID.getMessage(), response);
     }
 
-    @GetMapping("/chattingList")
-    public ResponseDto<List<ChattingListResponseDto>> getChattingList(@RequestParam Long userId){
-
-        List<ChattingListResponseDto> response = chatRoomService.getChattingList(userId);
-
-        return ResponseDto.response(GET_CHATTING_LIST.getCode(), GET_CHATTING_LIST.getMessage(), response);
-    }
 
     @PostMapping("/test") // addListener 테스트 용
     public void addListener(@RequestParam Long roomId) {
@@ -63,10 +46,10 @@ public class ChatRoomController {
         log.info("addListener");
     }
 
-    @GetMapping("/test1") // addListener 테스트 용
-    public void test1(@RequestParam Long roomId) {
-        chatRoomService.hi(roomId);
-        log.info("test1");
-    }
+//    @GetMapping("/test1") // addListener 테스트 용
+//    public void test1(@RequestParam Long roomId) {
+//        chatRoomService.hi(roomId);
+//        log.info("test1");
+//    }
 
 }
--- src/main/java/com/leets/X/domain/chat/service/ChatRoomService.java
+++ src/main/java/com/leets/X/domain/chat/service/ChatRoomService.java
@@ -1,26 +1,18 @@
 package com.leets.X.domain.chat.service;
 
 import com.leets.X.domain.chat.dto.request.ChatRoomCheckRequstDto;
-import com.leets.X.domain.chat.dto.request.GetChatRoomRequestDto;
 import com.leets.X.domain.chat.dto.request.FindChatRoomRequestDto;
 import com.leets.X.domain.chat.dto.response.*;
-import com.leets.X.domain.chat.entity.ChatMessage;
 import com.leets.X.domain.chat.entity.ChatRoom;
-import com.leets.X.domain.chat.exception.NotFoundChatRoomException;
 import com.leets.X.domain.chat.redis.RedisListener;
-import com.leets.X.domain.chat.repository.ChatMessageRepository;
 import com.leets.X.domain.chat.repository.ChatRoomRepository;
 import com.leets.X.domain.user.domain.User;
 import com.leets.X.domain.user.exception.UserNotFoundException;
 import com.leets.X.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -29,7 +21,6 @@ public class ChatRoomService {
 
     private final RedisListener redisMessageListener;
     private final ChatRoomRepository chatRoomRepository;
-    private final ChatMessageRepository chatMessageRepository;
     private final UserRepository userRepository;
 
     public ChatRoomResponseDto saveChatRoom(FindChatRoomRequestDto findChatRoomRequestDto) {
@@ -52,21 +43,7 @@ public class ChatRoomService {
         }
     }
 
-    public ChattingDto getChatRoom(GetChatRoomRequestDto getChatRoomRequestDto) {
-        ChatRoom findRoom = chatRoomRepository.findById(getChatRoomRequestDto.roomId())
-                .orElseThrow(NotFoundChatRoomException::new);
-        User user1 = findRoom.getUser1();
-        User user2 = findRoom.getUser2();
 
-        List<ChatMessageResponseDto> chatMessageList = chatMessageRepository.findByRoomIdOrderByCreatedAtDesc(
-                getChatRoomRequestDto.roomId(), PageRequest.of(getChatRoomRequestDto.page()- 1, getChatRoomRequestDto.size()))
-                .getContent()
-                .stream()
-                .map(ChatMessageResponseDto::fromEntity)
-                .collect(Collectors.toList());
-
-        return new ChattingDto(user1.getId(), user2.getId(), user1.getCustomId(), user2.getCustomId(), chatMessageList);
-    }
 
     public ChatRoomResponseDto findUser1User2ChatRoom(ChatRoomCheckRequstDto checkDto) {
         Long result = chatRoomRepository.findRoomIdByUserIds(checkDto.user1Id(), checkDto.user2Id()).orElse(-1L);
@@ -78,26 +55,10 @@ public class ChatRoomService {
         redisMessageListener.adaptMessageListener(roomId); // 리스너 등록
     }
 
-    public List<ChattingListResponseDto> getChattingList(Long userId) {
-        List<ChatRoom> chatRooms = chatRoomRepository.findRoomIdByUserId(userId)
-                .orElseThrow(NotFoundChatRoomException::new);
 
-        return chatRooms.stream()
-                .map(chatRoom -> {
-                    ChatMessage latestMessage = chatMessageRepository.findTopByRoomIdOrderByCreatedAtDesc(chatRoom.getId()).orElse(null);
-                    LatestMessageDto latestMessageDto = (latestMessage != null)
-                            ? LatestMessageDto.of(latestMessage)
-                            : new LatestMessageDto("", null);
-
-                    return ChattingListResponseDto.of(chatRoom, latestMessageDto);
-                })
-                .collect(Collectors.toList());
-    }
-
-
-    public void hi(Long roomId){
-        ChatMessage chatMessage = chatMessageRepository.findTopByRoomIdOrderByCreatedAtDesc(roomId).get();
-        System.out.println(chatMessage.getRoomId() + " " + chatMessage.getCreatedAt() + " " + chatMessage.getContent());
-    }
+//    public void hi(Long roomId){
+//        ChatMessage chatMessage = chatMessageRepository.findTopByRoomIdOrderByCreatedAtDesc(roomId).get();
+//        System.out.println(chatMessage.getRoomId() + " " + chatMessage.getCreatedAt() + " " + chatMessage.getContent());
+//    }
 
 }
