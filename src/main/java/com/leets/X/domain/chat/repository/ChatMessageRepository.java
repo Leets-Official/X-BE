9a02e838586c4653b142de47ad171acf42ca8fe0
koreaioi <koreaioi@gachon.ac.kr>
2024-11-02 23:15:25 +0900

    feat: add Method findTopByRoomIdOrderByCreatedAtDesc() to ChatMessageRepository

--- src/main/java/com/leets/X/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/leets/X/domain/chat/repository/ChatMessageRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
 
     Page<ChatMessage> findByRoomIdOrderByCreatedAtDesc(Long roomId, Pageable pageable);
+
+    Optional<ChatMessage> findTopByRoomIdOrderByCreatedAtDesc(Long roomId);
+
 
 }
