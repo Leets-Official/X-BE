db0b8593f47fc18c5a6406c23d6cef0da8eeabed
koreaioi <koreaioi@gachon.ac.kr>
2024-11-02 23:11:27 +0900

    fix: rename ChatMessageController -> PublishMessageController

--- src/main/java/com/leets/X/domain/chat/controller/PublishMessageController.java
+++ src/main/java/com/leets/X/domain/chat/controller/PublishMessageController.java
@@ -2,7 +2,7 @@ package com.leets.X.domain.chat.controller;
 
 
 import com.leets.X.domain.chat.dto.request.MessageDto;
-import com.leets.X.domain.chat.service.ChatMessageService;
+import com.leets.X.domain.chat.service.PublishMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-public class ChatMessageController {
+public class PublishMessageController {
 
-    private final ChatMessageService chatMessageService;
+    private final PublishMessageService publishMessageService;
 
-    // 클라이언트는 /pub/chats/messages/ + roomId로 보낸다.
-    @MessageMapping("/chats/messages/{room-id}")
+    // 클라이언트는 "/pub/chats/messages" 로 보낸다.
+    @MessageMapping("/chats/messages")
     public void message(MessageDto messageDto) {
-        chatMessageService.publishMessage(messageDto);
+        publishMessageService.publishMessage(messageDto);
     }
 }
