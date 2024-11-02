642f5d000792133b9a2835d2573f9a8ea470f89f
koreaioi <koreaioi@gachon.ac.kr>
2024-11-03 02:53:05 +0900

    feat: ChattingController, Service 분리

---
+++ src/main/java/com/leets/X/domain/chat/controller/ChattingController.java
@@ -0,0 +1,37 @@
+package com.leets.X.domain.chat.controller;
+
+import com.leets.X.domain.chat.dto.request.GetChatRoomRequestDto;
+import com.leets.X.domain.chat.dto.response.ChattingDto;
+import com.leets.X.domain.chat.dto.response.ChattingListResponseDto;
+import com.leets.X.domain.chat.service.ChattingService;
+import com.leets.X.global.common.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.leets.X.domain.chat.controller.ResponseMessage.GET_CHATROOM;
+import static com.leets.X.domain.chat.controller.ResponseMessage.GET_CHATTING_LIST;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class ChattingController {
+
+    private final ChattingService chattingService;
+
+    // 채팅방 하나를 조회해준다. (대화 내역을 돌려준다는 의미)
+    @GetMapping("/chatting")
+    public ResponseDto<ChattingDto> findChatting( @RequestBody GetChatRoomRequestDto getChatRoomRequestDto){
+        ChattingDto response = chattingService.getChatRoom(getChatRoomRequestDto);
+        return ResponseDto.response(GET_CHATROOM.getCode(), GET_CHATROOM.getMessage(), response);
+    }
+
+
+    @GetMapping("/chattingList")
+    public ResponseDto<List<ChattingListResponseDto>> findChattingList(@RequestParam Long userId){
+        List<ChattingListResponseDto> response = chattingService.getChattingList(userId);
+        return ResponseDto.response(GET_CHATTING_LIST.getCode(), GET_CHATTING_LIST.getMessage(), response);
+    }
+
+}
---
+++ src/main/java/com/leets/X/domain/chat/service/ChattingService.java
@@ -0,0 +1,60 @@
+package com.leets.X.domain.chat.service;
+
+import com.leets.X.domain.chat.dto.request.GetChatRoomRequestDto;
+import com.leets.X.domain.chat.dto.response.ChatMessageResponseDto;
+import com.leets.X.domain.chat.dto.response.ChattingDto;
+import com.leets.X.domain.chat.dto.response.ChattingListResponseDto;
+import com.leets.X.domain.chat.dto.response.LatestMessageDto;
+import com.leets.X.domain.chat.entity.ChatMessage;
+import com.leets.X.domain.chat.entity.ChatRoom;
+import com.leets.X.domain.chat.exception.NotFoundChatRoomException;
+import com.leets.X.domain.chat.repository.ChatMessageRepository;
+import com.leets.X.domain.chat.repository.ChatRoomRepository;
+import com.leets.X.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ChattingService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatMessageRepository chatMessageRepository;
+
+    public ChattingDto getChatRoom(GetChatRoomRequestDto getChatRoomRequestDto) {
+        ChatRoom findRoom = chatRoomRepository.findById(getChatRoomRequestDto.roomId())
+                .orElseThrow(NotFoundChatRoomException::new);
+        User user1 = findRoom.getUser1();
+        User user2 = findRoom.getUser2();
+
+        List<ChatMessageResponseDto> chatMessageList = chatMessageRepository.findByRoomIdOrderByCreatedAtDesc(
+                        getChatRoomRequestDto.roomId(), PageRequest.of(getChatRoomRequestDto.page()- 1, getChatRoomRequestDto.size()))
+                .getContent()
+                .stream()
+                .map(ChatMessageResponseDto::fromEntity)
+                .collect(Collectors.toList());
+
+        return new ChattingDto(user1.getId(), user2.getId(), user1.getCustomId(), user2.getCustomId(), chatMessageList);
+    }
+
+    public List<ChattingListResponseDto> getChattingList(Long userId) { // 추후 JWT 파싱으로 받아내기.
+        List<ChatRoom> chatRooms = chatRoomRepository.findRoomIdByUserId(userId)
+                .orElseThrow(NotFoundChatRoomException::new);
+
+        return chatRooms.stream()
+                .map(chatRoom -> {
+                    ChatMessage latestMessage = chatMessageRepository.findTopByRoomIdOrderByCreatedAtDesc(chatRoom.getId()).orElse(null);
+                    LatestMessageDto latestMessageDto = (latestMessage != null)
+                            ? LatestMessageDto.of(latestMessage)
+                            : new LatestMessageDto("", null);
+
+                    return ChattingListResponseDto.of(chatRoom, latestMessageDto);
+                })
+                .collect(Collectors.toList());
+    }
+
+}
