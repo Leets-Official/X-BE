23f49c7b6ab4d9a5ea229a922d55eb0ee73e3208
seokjun01 <suckjungod@naver.com>
2024-11-03 15:35:57 +0900

    Feat: 응답 성공 메시지 파일 생성

--- src/main/java/com/leets/X/domain/post/controller/ResponseMessage.java
+++ src/main/java/com/leets/X/domain/post/controller/ResponseMessage.java
@@ -1,4 +1,20 @@
 package com.leets.X.domain.post.controller;
 
-public class ResponseMessage {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResponseMessage {
+
+    POST_SUCCESS(201, "게시물이 성공적으로 생성되었습니다."),
+    GET_POST_SUCCESS(200, "게시물 조회에 성공했습니다."),
+    GET_SORTED_BY_LIKES_SUCCESS(200, "좋아요 순으로 게시물 조회에 성공했습니다."),
+    GET_LATEST_POST_SUCCESS(200, "최신 게시물 조회에 성공했습니다."),
+    ADD_LIKE_SUCCESS(201, "좋아요가 추가되었습니다."),
+    POST_DELETED_SUCCESS(200, "게시물이 성공적으로 삭제되었습니다."),
+    LIKE_CANCEL_SUCCESS(200, "좋아요가 성공적으로 취소되었습니다.");
+
+    private final int code;
+    private final String message;
 }
