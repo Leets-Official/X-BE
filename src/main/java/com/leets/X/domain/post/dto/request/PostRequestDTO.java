6eba64decc7a3f6e20b5d1f899e3e12c17b0c28c
seokjun01 <suckjungod@naver.com>
2024-11-04 20:48:10 +0900

    Refactor: record 타입으로 변경 후, 정적 팩토리 메서드로 요청dto 객체 반환

--- src/main/java/com/leets/X/domain/post/dto/request/PostRequestDTO.java
+++ src/main/java/com/leets/X/domain/post/dto/request/PostRequestDTO.java
@@ -1,24 +1,10 @@
 package com.leets.X.domain.post.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@Builder
-@Getter
-public class PostRequestDTO {
+public record PostRequestDTO(@NotBlank(message = "Content cannot be empty") String content) {
 
-    @JsonCreator
-    public PostRequestDTO(@JsonProperty("content") String content) {
-        this.content = content;
+    public static PostRequestDTO from(String content) {
+        return new PostRequestDTO(content);
     }
-    @NotBlank(message = "Content cannot be empty")
-    private String content;
-
-
-
 }
