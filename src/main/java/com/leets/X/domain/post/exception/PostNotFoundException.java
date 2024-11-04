565f96eec24cc7b4341af0f936d2ca35ea8d678a
seokjun01 <suckjungod@naver.com>
2024-11-04 20:49:35 +0900

    Refactor: 들여쓰기 수정

--- src/main/java/com/leets/X/domain/post/exception/PostNotFoundException.java
+++ src/main/java/com/leets/X/domain/post/exception/PostNotFoundException.java
@@ -3,7 +3,6 @@ package com.leets.X.domain.post.exception;
 import com.leets.X.global.common.exception.BaseException;
 
 import static com.leets.X.domain.post.exception.ErrorMessage.POST_NOT_FOUND;
-import static com.leets.X.domain.user.exception.ErrorMessage.USER_NOT_FOUND;
 
 public class PostNotFoundException extends BaseException {
 
