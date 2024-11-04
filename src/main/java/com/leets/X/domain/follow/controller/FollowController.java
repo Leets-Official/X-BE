5791bed35be6cc51f72ac6f7c02ebe8bd9131b31
hyxklee <ewgt1234@naver.com>
2024-11-04 15:56:02 +0900

    feat: 팔로우, 팔로워 조회, 팔로잉 조회 구현

---
+++ src/main/java/com/leets/X/domain/follow/controller/FollowController.java
@@ -0,0 +1,35 @@
+package com.leets.X.domain.follow.controller;
+
+import com.leets.X.domain.follow.dto.response.FollowResponse;
+import com.leets.X.domain.follow.service.FollowService;
+import com.leets.X.global.common.response.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.leets.X.domain.follow.controller.ResponseMessage.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/follows")
+public class FollowController {
+    private final FollowService followService;
+
+    @PostMapping("/{userId}")
+    public ResponseDto<String> follow(@PathVariable Long userId, @AuthenticationPrincipal String email){
+        followService.follow(userId, email);
+        return ResponseDto.response(FOLLOW_SUCCESS.getCode(), FOLLOW_SUCCESS.getMessage());
+    }
+
+    @GetMapping("follower/{userId}")
+    public ResponseDto<List<FollowResponse>> getFollowers(@PathVariable Long userId){
+        return ResponseDto.response(GET_FOLLOWER_SUCCESS.getCode(), GET_FOLLOWER_SUCCESS.getMessage(), followService.getFollowers(userId));
+    }
+
+    @GetMapping("following/{userId}")
+    public ResponseDto<List<FollowResponse>> getFollowings(@PathVariable Long userId){
+        return ResponseDto.response(GET_FOLLOWING_SUCCESS.getCode(), GET_FOLLOWING_SUCCESS.getMessage(), followService.getFollowings(userId));
+    }
+}
---
+++ src/main/java/com/leets/X/domain/follow/service/FollowService.java
@@ -0,0 +1,69 @@
+package com.leets.X.domain.follow.service;
+
+import com.leets.X.domain.follow.domain.Follow;
+import com.leets.X.domain.follow.dto.response.FollowResponse;
+import com.leets.X.domain.follow.exception.AlreadyFollowException;
+import com.leets.X.domain.follow.exception.InvalidFollowException;
+import com.leets.X.domain.follow.repository.FollowRepository;
+import com.leets.X.domain.user.domain.User;
+import com.leets.X.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+    private final FollowRepository followRepository;
+    private final UserService userService;
+
+    // email -> userId 방향. 팔로우 하는 사람이 follower
+    @Transactional
+    public void follow(Long userId, String email){
+        User follower = userService.find(email);
+        User followed = userService.find(userId);
+
+        check(follower.getId(), followed.getId());
+        // 저장
+        Follow follow = followRepository.save(Follow.of(follower, followed));
+    }
+
+    // user를 팔로우 하는 사람 리스트. user는 팔로우 당하는 사람이므로 followeId에 있어야함
+    public List<FollowResponse> getFollowers(Long userId){
+        User user = userService.find(userId);
+        List<Follow> followerList = followRepository.findByFollowedId(userId);
+        List<FollowResponse> followerResponses = new ArrayList<>();
+
+        return followerList.stream()
+                .map(follow -> {
+                    return FollowResponse.from(follow.getFollower());
+                })
+                .toList();
+    }
+
+    public List<FollowResponse> getFollowings(Long userId){
+        User user = userService.find(userId);
+        List<Follow> followerList = followRepository.findByFollowerId(userId);
+        List<FollowResponse> followerResponses = new ArrayList<>();
+
+        return followerList.stream()
+                .map(follow -> {
+                    return FollowResponse.from(follow.getFollowed());
+                })
+                .toList();
+    }
+
+    private void check(Long followerId, Long followedId){
+        if(followRepository.existsByFollowerIdAndFollowedId(followerId, followedId)){
+            throw new AlreadyFollowException();
+        }
+
+        if(followerId.equals(followedId)){
+            throw new InvalidFollowException();
+        }
+    }
+
+}
