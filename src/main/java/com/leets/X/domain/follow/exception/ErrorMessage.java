2954eda31f72a822249686aae49b80857dfa41cb
hyxklee <ewgt1234@naver.com>
2024-11-04 16:21:39 +0900

    feat: 팔로우 되지 않은 상대를 언팔로우 할 때 예외 구현

--- src/main/java/com/leets/X/domain/follow/exception/ErrorMessage.java
+++ src/main/java/com/leets/X/domain/follow/exception/ErrorMessage.java
@@ -9,7 +9,10 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     ALREADY_FOLLOW(400,"이미 팔로우한 상태입니다."),
-    INVALID_FOLLOW(400, "자기 자신은 팔로우 할 수 없습니다.");
+    INVALID_FOLLOW(400, "자기 자신은 팔로우 할 수 없습니다."),
+    FOLLOW_NOT_FOUND(404, "팔로우 데이터를 찾을 수 없습니다."),
+    INVALID_UNFOLLOW(400, "팔로우 하지 않은 상대는 언팔로우 할 수 없습니다.");
+
 
     private final int code;
     private final String message;
