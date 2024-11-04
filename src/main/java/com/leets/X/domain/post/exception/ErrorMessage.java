f264c7fe4491ebb3e5c82ad2bd8979d4fb74374b
seokjun01 <suckjungod@naver.com>
2024-11-04 20:45:47 +0900

    Refactor: 삭제권한, 좋아요 눌림 여부, 이미 좋아요 눌렀는지에 대한 응답 코드와 메시지 추가

--- src/main/java/com/leets/X/domain/post/exception/ErrorMessage.java
+++ src/main/java/com/leets/X/domain/post/exception/ErrorMessage.java
@@ -7,8 +7,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorMessage {
 
-    POST_NOT_FOUND(404,"존재하지 않는 게시글입니다.");
-
+    POST_NOT_FOUND(404, "존재하지 않는 게시글입니다."),
+    UNAUTHORIZED_POST_DELETION(403, "게시물을 삭제할 권한이 없습니다."),
+    ALREADY_LIKED(400, "이미 좋아요를 누른 게시물입니다."),
+    NOT_LIKED(400, "좋아요가 눌려 있지 않은 게시물입니다.");
     private final int code;
     private final String message;
 
