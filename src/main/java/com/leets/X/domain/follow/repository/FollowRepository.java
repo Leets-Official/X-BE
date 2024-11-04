a8cbeec530709abba7166b22d7808b1932692f64
hyxklee <ewgt1234@naver.com>
2024-11-04 16:22:24 +0900

    feat: 팔로우 단건 조회

--- src/main/java/com/leets/X/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/leets/X/domain/follow/repository/FollowRepository.java
@@ -4,8 +4,11 @@ import com.leets.X.domain.follow.domain.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    Optional<Follow> findByFollowerIdAndFollowedId(Long followerId, Long followedId);
 
     // followerId가 포함된 객체 리스트 반환
     List<Follow> findByFollowerId(Long followerId);
