d7d4464a79341108abfe31b23c79cf4b49086f44
koreaioi <koreaioi@gachon.ac.kr>
2024-11-06 01:33:38 +0900

    refactor: validate, generateChatRoomMessages 리팩토링

--- src/main/java/com/leets/X/domain/chat/service/ChattingService.java
+++ src/main/java/com/leets/X/domain/chat/service/ChattingService.java
@@ -26,35 +26,46 @@ public class ChattingService {
     private final ChatMessageRepository chatMessageRepository;
 
     public ChattingDto getChatRoom(GetChatRoomRequestDto getChatRoomRequestDto) {
-        ChatRoom findRoom = chatRoomRepository.findById(getChatRoomRequestDto.roomId())
-                .orElseThrow(NotFoundChatRoomException::new);
+        ChatRoom findRoom = validateChatRoom(getChatRoomRequestDto);
         User user1 = findRoom.getUser1();
         User user2 = findRoom.getUser2();
 
-        List<ChatMessageResponseDto> chatMessageList = chatMessageRepository.findByRoomIdOrderByCreatedAtDesc(
-                        getChatRoomRequestDto.roomId(), PageRequest.of(getChatRoomRequestDto.page()- 1, getChatRoomRequestDto.size()))
-                .getContent()
-                .stream()
-                .map(ChatMessageResponseDto::fromEntity)
-                .collect(Collectors.toList());
-
+        List<ChatMessageResponseDto> chatMessageList = generateChatRoomMessages(getChatRoomRequestDto);
         return new ChattingDto(user1.getId(), user2.getId(), user1.getCustomId(), user2.getCustomId(), chatMessageList);
     }
 
+
     public List<ChattingListResponseDto> getChattingList(Long userId) { // 추후 JWT 파싱으로 받아내기.
-        List<ChatRoom> chatRooms = chatRoomRepository.findRoomsByUserId(userId)
-                .orElseThrow(NotFoundChatRoomException::new);
+        List<ChatRoom> chatRooms = validateChatRommList(userId);
 
         return chatRooms.stream()
                 .map(chatRoom -> {
                     ChatMessage latestMessage = chatMessageRepository.findTopByRoomIdOrderByCreatedAtDesc(chatRoom.getId()).orElse(null);
                     LatestMessageDto latestMessageDto = (latestMessage != null)
-                            ? LatestMessageDto.of(latestMessage)
-                            : new LatestMessageDto("", null);
-
+                            ? LatestMessageDto.of(latestMessage) : new LatestMessageDto("", null);
                     return ChattingListResponseDto.of(chatRoom, latestMessageDto);
                 })
                 .collect(Collectors.toList());
+    }
+
+    private List<ChatMessageResponseDto> generateChatRoomMessages(GetChatRoomRequestDto getChatRoomRequestDto) {
+        return chatMessageRepository.findByRoomIdOrderByCreatedAtDesc(
+                        getChatRoomRequestDto.roomId(), PageRequest.of(getChatRoomRequestDto.page()- 1, getChatRoomRequestDto.size()))
+                .getContent()
+                .stream()
+                .map(ChatMessageResponseDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+
+    private ChatRoom validateChatRoom(GetChatRoomRequestDto getChatRoomRequestDto) {
+        return chatRoomRepository.findById(getChatRoomRequestDto.roomId())
+                .orElseThrow(NotFoundChatRoomException::new);
+    }
+
+    private List<ChatRoom> validateChatRommList(Long userId) {
+        return chatRoomRepository.findRoomsByUserId(userId)
+                .orElseThrow(NotFoundChatRoomException::new);
     }
 
 }
