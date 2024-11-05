6718cb4f74e7374c4bd9fd5c9be1d3448e5ecc0d
seokjun01 <suckjungod@naver.com>
2024-11-05 22:19:44 +0900

    Refactor: 서비스와 컨트롤러계층의 책임 분리

--- src/main/java/com/leets/X/domain/post/controller/PostController.java
+++ src/main/java/com/leets/X/domain/post/controller/PostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+//컨트롤러에서 ResponseDto만들게끔
 @Tag(name = "POST")
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -26,21 +27,24 @@ public class PostController {
     @GetMapping("/{id}")
     @Operation(summary = "게시물 ID로 조회")
     public ResponseDto<PostResponseDto> getPost(@PathVariable Long id) {
-        return postService.getPostResponse(id);
+        PostResponseDto postResponseDto = postService.getPostResponse(id);
+        return ResponseDto.response(ResponseMessage.GET_POST_SUCCESS.getCode(), ResponseMessage.GET_POST_SUCCESS.getMessage(), postResponseDto);
     }
 
 
     @GetMapping("/likes")
     @Operation(summary = "좋아요 수로 정렬한 게시물 조회")
     public ResponseDto<List<PostResponseDto>> getPostsSortedByLikes() {
-        return postService.getPostsSortedByLikes();
+        List<PostResponseDto> posts = postService.getPostsSortedByLikes();
+        return ResponseDto.response(ResponseMessage.GET_SORTED_BY_LIKES_SUCCESS.getCode(), ResponseMessage.GET_SORTED_BY_LIKES_SUCCESS.getMessage(), posts);
     }
 
-    
+
     @GetMapping("/latest")
     @Operation(summary = "최신 게시물 조회")
     public ResponseDto<List<PostResponseDto>> getLatestPosts() {
-        return postService.getLatestPosts();
+        List<PostResponseDto> posts = postService.getLatestPosts();
+        return ResponseDto.response(ResponseMessage.GET_LATEST_POST_SUCCESS.getCode(), ResponseMessage.GET_LATEST_POST_SUCCESS.getMessage(), posts);
     }
 
 
@@ -48,28 +52,31 @@ public class PostController {
     @Operation(summary = "글 생성")
     public ResponseDto<PostResponseDto> createPost(@RequestBody PostRequestDTO postRequestDTO, @AuthenticationPrincipal String email) {
         // 인증된 사용자의 이메일을 `@AuthenticationPrincipal`을 통해 주입받음
-        return postService.createPost(postRequestDTO, email);
+        PostResponseDto postResponseDto = postService.createPost(postRequestDTO, email);
+        return ResponseDto.response(ResponseMessage.POST_SUCCESS.getCode(), ResponseMessage.POST_SUCCESS.getMessage(), postResponseDto);
     }
-
 
     @PostMapping("/{postId}/like")
     @Operation(summary = "게시물에 좋아요 추가")
     public ResponseDto<String> addLike(@PathVariable Long postId, @AuthenticationPrincipal String email) {
-        return postService.addLike(postId, email);
+        String responseMessage = postService.addLike(postId, email);
+        return ResponseDto.response(ResponseMessage.ADD_LIKE_SUCCESS.getCode(), responseMessage);
     }
 
 
     @DeleteMapping("/{postId}")
     @Operation(summary = "게시물 삭제")
     public ResponseDto<String> deletePost(@PathVariable Long postId, @AuthenticationPrincipal String email) {
-        return postService.deletePost(postId, email);
+        String responseMessage = postService.deletePost(postId, email);
+        return ResponseDto.response(ResponseMessage.POST_DELETED_SUCCESS.getCode(), responseMessage);
     }
 
 
     @DeleteMapping("/{postId}/like")
     @Operation(summary = "좋아요 취소")
     public ResponseDto<String> cancelLike(@PathVariable Long postId, @AuthenticationPrincipal String email) {
-        return postService.cancelLike(postId, email);
+        String responseMessage = postService.cancelLike(postId, email);
+        return ResponseDto.response(ResponseMessage.LIKE_CANCEL_SUCCESS.getCode(), responseMessage);
     }
 
 
