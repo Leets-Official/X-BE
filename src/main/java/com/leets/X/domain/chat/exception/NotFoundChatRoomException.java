b467f49aa6e163995652e31ef7b1ffd05ff9b28c
koreaioi <koreaioi@gachon.ac.kr>
2024-11-11 23:05:08 +0900

    refactor: ErrorMessgae 변수 명 네이밍 규칙 반영

--- src/main/java/com/leets/X/domain/chat/exception/NotFoundChatRoomException.java
+++ src/main/java/com/leets/X/domain/chat/exception/NotFoundChatRoomException.java
@@ -1,10 +1,10 @@
 package com.leets.X.domain.chat.exception;
 
 import com.leets.X.global.common.exception.BaseException;
-import static com.leets.X.domain.chat.exception.ErrorMessage.NOT_FOUND_CHATROOM;
+import static com.leets.X.domain.chat.exception.ErrorMessage.CHATROOM_NOT_FOUND;
 
 public class NotFoundChatRoomException extends BaseException {
     public NotFoundChatRoomException() {
-        super(NOT_FOUND_CHATROOM.getCode(), NOT_FOUND_CHATROOM.getMessage());
+        super(CHATROOM_NOT_FOUND.getCode(), CHATROOM_NOT_FOUND.getMessage());
     }
 }
