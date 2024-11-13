1423ff6331ae357ec234a1aa84b8ff45903bcd18
hyxklee <ewgt1234@naver.com>
2024-11-14 04:58:37 +0900

    HOTFIX: 팔로우 목록 조회시 프로필 이미지 반환

--- src/main/java/com/leets/X/domain/follow/dto/response/FollowResponse.java
+++ src/main/java/com/leets/X/domain/follow/dto/response/FollowResponse.java
@@ -1,22 +1,30 @@
 package com.leets.X.domain.follow.dto.response;
 
+import com.leets.X.domain.image.domain.Image;
+import com.leets.X.domain.image.dto.request.ImageDto;
+import com.leets.X.domain.post.dto.response.PostUserResponse;
 import com.leets.X.domain.user.domain.User;
 import lombok.Builder;
 
 @Builder
 public record FollowResponse(
         Long id,
-        String name,
-        String customId,
-        String introduce
+        PostUserResponse response
 //        boolean followStatus
 ) {
     public static FollowResponse from(User user) {
         return FollowResponse.builder()
                 .id(user.getId())
-                .name(user.getName())
-                .customId(user.getCustomId())
-                .introduce(user.getIntroduce())
+                .response(toPostUserResponse(user))
                 .build();
+    }
+
+    public static PostUserResponse toPostUserResponse(User user) {
+        ImageDto dto = null;
+        Image image = user.getImage();
+        if(image != null){
+            dto = ImageDto.of(image.getName(), image.getUrl());
+        }
+        return PostUserResponse.from(user, dto);
     }
 }
