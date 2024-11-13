716607e9a1d2d429ffe7018b37f628b782f48e2e
koreaioi <koreaioi@gachon.ac.kr>
2024-11-13 18:46:28 +0900

    fix: ChattingController 인증 유저 정보 사용

--- src/main/java/com/leets/X/domain/chat/controller/ChattingController.java
+++ src/main/java/com/leets/X/domain/chat/controller/ChattingController.java
@@ -7,6 +7,7 @@ import com.leets.X.global.common.response.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,16 +25,17 @@ public class ChattingController {
 
     @GetMapping("/chatting/{roomId}/{page}/{size}")
     @Operation(summary = "하나의 채팅방 + 해당 채팅 내역 조회")
-    public ResponseDto<ChattingDto> findChatting(@PathVariable Long roomId, @PathVariable Integer page, @PathVariable Integer size) {
-        ChattingDto response = chattingService.getChatRoom(roomId, page, size);
+    public ResponseDto<ChattingDto> findChatting(@PathVariable Long roomId, @PathVariable Integer page,
+                                                 @PathVariable Integer size, @AuthenticationPrincipal String email) {
+        ChattingDto response = chattingService.getChatRoom(roomId, page, size, email);
         return ResponseDto.response(CHATROOM_GET.getCode(), CHATROOM_GET.getMessage(), response);
     }
 
 
     @GetMapping("/chattingList/{userId}")
     @Operation(summary = "유저가 속한 모든 채팅방 조회")
-    public ResponseDto<List<ChattingListResponseDto>> findChattingList(@PathVariable Long userId){
-        List<ChattingListResponseDto> response = chattingService.getChattingList(userId);
+    public ResponseDto<List<ChattingListResponseDto>> findChattingList(@PathVariable Long userId, @AuthenticationPrincipal String email){
+        List<ChattingListResponseDto> response = chattingService.getChattingList(userId, email);
         return ResponseDto.response(CHATTINGLIST_GET.getCode(), CHATTINGLIST_GET.getMessage(), response);
     }
 
